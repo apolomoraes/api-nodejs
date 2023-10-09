dc5ef78052bfb190f3f042f575de6098c1cdc3e4
Apolo Moraes <vlogsapolo@gmail.com>
2023-10-09 18:22:13 -0300

    added post route and added get route

--- server.js
+++ server.js
@@ -6,11 +6,21 @@ const server = fastify();
 const database = new DatabaseMemory();
 
 server.post('/videos', (req, res) => {
+  const { title, description, duration } = req.body
 
+  database.create({
+    title,
+    description,
+    duration,
+  })
+
+  return res.status(201).send()
 })
 
-server.get('/videos', (req, res) => {
+server.get('/videos', () => {
+  const videos = database.list();
 
+  return videos
 })
 
 server.put('/videos/:id', (req, res) => {
