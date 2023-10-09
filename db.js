85a8a36fa9cf5aa770ddf858b86945514d25375a
Apolo Moraes <vlogsapolo@gmail.com>
2023-10-09 20:03:41 -0300

    added database config

--- db.js
+++ db.js
@@ -1,5 +1,16 @@
+import 'dotenv/config';
 import postgres from "postgres";
 
-const sql = postgres({})
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-export default sql
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
